98bf652a11889c09dcd0476c889818c2fc3f79f6
roman <roman.rahimov@gmail.com>
2021-03-18 18:25:21 +0300

    Освобождение коннекта при psycopg2.OperationalError

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_400_BAD_REQUEST
 import app.settings as settings
 import psycopg2
 from psycopg2 import pool, extras
 from fastapi import Depends
-
+from fastapi.logger import logger
+from app.models import ErrorMessage
 
 dbpool = psycopg2.pool.ThreadedConnectionPool(settings.db_min_connections, settings.db_max_connections,
                                               user=settings.dbusr, password=settings.dbpwd,
@@ -14,10 +18,6 @@ async def get_db_connection():
     try:
         yield connection
         connection.commit()
-    except psycopg2.OperationalError as error:
-        connection.rollback()
-        connection.close()
-        raise error
     finally:
         dbpool.putconn(connection)
 
@@ -28,3 +28,22 @@ async def get_db_cursor(connection=Depends(get_db_connection)):
         yield cursor
     finally:
         cursor.close()
+
+
+def check_if_error(obj):
+    if type(obj) is ErrorMessage:
+        return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content=jsonable_encoder(obj))
+    else:
+        return obj
+
+
+def handle_database_exception(connection, exc):
+    if type(exc) is psycopg2.errors.RaiseException:  # noqa
+        connection.rollback()
+        return ErrorMessage(message=str(exc).partition("\n")[0])
+    elif type(exc) is psycopg2.OperationalError:
+        connection.close()
+        logger.error(f"Database exception: {exc}")
+        return ErrorMessage(message="try later...")
+    else:
+        raise exc
--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK
 import hashlib
 import app.settings as settings
 from app.token import create
-from app.database import get_db_cursor
+from app.database import get_db_cursor, handle_database_exception, check_if_error
 from app.models import JWTToken, UserCredentials, ErrorMessage, Fingerprint
 
 tags_metadata = [
@@ -37,36 +37,39 @@ app = FastAPI(
                            "- **1**: ошибка авторизации, информация в **message**  \n"
                            "- **2**: срок действия пароля истёк, требуется его смена"}})
 async def auth_login(usercredentials: UserCredentials, cursor=Depends(get_db_cursor)):
-    cursor.execute("select * from auth.login(%s,%s,%s)",
-                   (usercredentials.login,
-                    hashlib.sha512(usercredentials.password.encode('utf-8')).hexdigest(),
-                    usercredentials.fingerprint))
-    res = cursor.fetchone()
-    if res["user_id"]:
-        if res["user_id"] < 0:
-            return JSONResponse(status_code=HTTP_401_UNAUTHORIZED,
-                                content=jsonable_encoder(ErrorMessage(code=2, message=res["error_message"])))
+    try:
+        cursor.execute("select * from auth.login(%s,%s,%s)",
+                       (usercredentials.login,
+                        hashlib.sha512(usercredentials.password.encode('utf-8')).hexdigest(),
+                        usercredentials.fingerprint))
+        res = cursor.fetchone()
+        if res["user_id"]:
+            if res["user_id"] < 0:
+                return JSONResponse(status_code=HTTP_401_UNAUTHORIZED,
+                                    content=jsonable_encoder(ErrorMessage(code=2, message=res["error_message"])))
+            else:
+                j = JSONResponse(content=jsonable_encoder(
+                    JWTToken(access_token=create({"iss": settings.api_domain,
+                                                  "sub": res["user_id"],
+                                                  "name": res["user_name"],
+                                                  "exp": res["access_token_lifetime"],
+                                                  "customer_code": res["customer_code"],
+                                                  "customer_name": res["customer_name"],
+                                                  "services": res["services"]}),
+                             token_type="Bearer")
+                ))
+                j.set_cookie(key="refresh_token",
+                             value=res["refresh_token"],
+                             httponly=True,
+                             domain=settings.api_domain,
+                             path=settings.api_path,
+                             secure=True)
+                return j
         else:
-            j = JSONResponse(content=jsonable_encoder(
-                JWTToken(access_token=create({"iss": settings.api_domain,
-                                              "sub": res["user_id"],
-                                              "name": res["user_name"],
-                                              "exp": res["access_token_lifetime"],
-                                              "customer_code": res["customer_code"],
-                                              "customer_name": res["customer_name"],
-                                              "services": res["services"]}),
-                         token_type="Bearer")
-            ))
-            j.set_cookie(key="refresh_token",
-                         value=res["refresh_token"],
-                         httponly=True,
-                         domain=settings.api_domain,
-                         path=settings.api_path,
-                         secure=True)
-            return j
-    else:
-        return JSONResponse(status_code=HTTP_401_UNAUTHORIZED,
-                            content=jsonable_encoder(ErrorMessage(code=1, message=res["error_message"])))
+            return JSONResponse(status_code=HTTP_401_UNAUTHORIZED,
+                                content=jsonable_encoder(ErrorMessage(code=1, message=res["error_message"])))
+    except Exception as exc:
+        return check_if_error(handle_database_exception(cursor.connection, exc))
 
 
 @app.post("/logout", response_class=Response, tags=["Auth"],
