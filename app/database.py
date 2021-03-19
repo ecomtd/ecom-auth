1980683e3762442777973801ddc28beca8490ad6
roman <roman.rahimov@gmail.com>
2021-03-19 17:10:36 +0300

    Освобождение коннекта при psycopg2.OperationalError

--- app/database.py
+++ app/database.py
@@ -8,6 +8,9 @@ from fastapi import Depends
 from fastapi.logger import logger
 from app.models import ErrorMessage
 
+
+logger.info(f"Database pool minconn=: {settings.db_min_connections}")
+logger.info(f"Database pool maxconn=: {settings.db_max_connections}")
 dbpool = psycopg2.pool.ThreadedConnectionPool(minconn=settings.db_min_connections, maxconn=settings.db_max_connections,
                                               user=settings.dbusr, password=settings.dbpwd,
                                               host=settings.dbip, port=settings.dbport, database=settings.dbname)
